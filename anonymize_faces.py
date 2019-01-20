d67678a368d2a0356b2269ca107fa7dc644352a4
RichyWalter <richardwalter@web.de>
2019-01-20 02:10:34 +0100

    added funczion to pixelate face (not working yet)

--- anonymize_faces.py
+++ anonymize_faces.py
@@ -1,40 +1,55 @@
-#cli based tool to anonymize faces
+# cli based tool to anonymize faces
 
-#imports
+# imports
 from mtcnn.mtcnn import MTCNN
 import cv2
 import numpy as np
 
 
-#init the face detection module
+# init the face detection module
 detector = MTCNN()
 
-#TODO hardcoded values, fix that with relative values
+# TODO hardcoded values, fix that with relative values
 def draw_pornobalken(l_eye, r_eye, img):
     l_eye = (l_eye[0]- 10, l_eye[1] + 10)
     r_eye = (r_eye[0]+ 10, r_eye[1] - 10)
     cv2.rectangle(img,l_eye,r_eye,(0,0,0),-1)
     return img
 
-#detect and anonymize faces on an image/frame
+def pixelate_face(img, face):
+    heigth = img.shape[0]
+    width = img.shape[1]
+    pixel_size = max(heigth, width)/100
+    for i in range(0, heigth-int(pixel_size), int(pixel_size)):
+        for y in range(int(pixel_size)):
+            img[i+y] = img[i]
+    return img
+
+
+
+
+# detect and anonymize faces on an image/frame
 def detect_and_anonymize(img):
-    #detect faces on the image/frame
+    # detect faces on the image/frame
     faces = detector.detect_faces(img)
     ano_img = img
-    #draw black rectangles on each face
+    # draw black rectangles on each face
     for face in faces:
         left_eye = face["keypoints"]["left_eye"]
         right_eye = face["keypoints"]["right_eye"]
-        ano_img = draw_pornobalken(left_eye, right_eye, ano_img)
+        bnd_box = face["box"]
+        #ano_img = draw_pornobalken(left_eye, right_eye, ano_img)
+        ano_img = pixelate_face(ano_img, bnd_box)
+        
     return ano_img 
 
 
-MODE = "webcam"  #webcam, image_folder
+MODE = "image"  # webcam, image_folder, image
 
 if(MODE == "image"):
     single_img = cv2.imread("image_data/testbild.jpg")
     result = detect_and_anonymize(single_img)
-    #show result
+    # show result
     cv2.imshow('result',result)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
